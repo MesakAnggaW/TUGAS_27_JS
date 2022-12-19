7fe8a2129b2d28a9d3c61a531a4896acad453dfe
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-19 09:51:29 +0700

    Mengerjakan Tugas 27 Javascript

---
+++ tugas27.js
@@ -0,0 +1,11 @@
+let kata = "No ";
+let kata2 = "rumah saya ";
+let kata3 = 30;
+let kata4 = " A";
+let kata5 = "Jalan Pangeran Tirtayasa No";
+let kata6 = 70;
+
+let kalimat = kata + kata2 + String(kata3) + kata4;
+
+console.log(kalimat);
+console.log(kata5, String(kata6));
